112c6e78bcbedd4db799619f35b97cd209f9cdd1
Claire Thallon <clairethallon@gmail.com>
2021-11-19 09:11:12 -0600

    confirmation modal working

--- src/components/Review/Review.jsx
+++ src/components/Review/Review.jsx
@@ -2,12 +2,26 @@ import React from "react";
 import { useDispatch, useSelector } from 'react-redux';
 import { useState, useEffect } from 'react';
 import Button from '@mui/material/Button';
-import { TextField } from '@mui/material/TextField';
 import { List } from '@mui/material';
 import { ListItem } from '@mui/material';
 import axios from 'axios';
 import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
-import Modal from 'react-bootstrap/Modal';
+import Modal from '@mui/material/Modal';
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
+
+
+const style = {
+    position: 'absolute',
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)',
+    width: 400,
+    bgcolor: 'background.paper',
+    border: '2px solid #000',
+    boxShadow: 24,
+    p: 4,
+};
 
 
 function Review() {
@@ -20,9 +34,9 @@ function Review() {
 
     const dispatch = useDispatch();
 
-    // const [show, setShow] = useState(false);
-    // const handleClose = () => setShow(false);
-    // const handleShow = () => setShow(true);
+    const [show, setShow] = useState(false);
+    const handleClose = () => setShow(false);
+    const handleShow = () => setShow(true);
 
     const submitFeedback = () => {
         let objectToSend = {
@@ -34,17 +48,21 @@ function Review() {
         console.log('sending', objectToSend);
         axios.post('/api/feedback', objectToSend).then((response) => {
             console.log('post successful', response.data);
+            handleShow();
         }).catch((err) => {
             console.log(err);
             alert('nah');
         });
+
+
+    }
+
+    const clearResponses = () => {
         dispatch({
             type: 'EMPTY',
             payload: ''
         });
-        // handleShow();
     }
-
 
     return (
         <div>
@@ -57,22 +75,23 @@ function Review() {
                     <ListItem>Support:  {support}/5</ListItem>
                     <ListItem>Comments:  {comments}</ListItem>
                 </List>
-                <Link to="/"><Button variant="contained" onClick={submitFeedback}>Submit</Button></Link>
-                {/* <Modal
-                    open={open}
+                <Button variant="contained" onClick={submitFeedback}>Submit</Button>
+                <Modal
+                    open={show}
                     onClose={handleClose}
                     aria-labelledby="modal-modal-title"
                     aria-describedby="modal-modal-description"
                 >
                     <Box sx={style}>
                         <Typography id="modal-modal-title" variant="h6" component="h2">
-                            Text in a modal
-    </Typography>
-                        <Typography id="modal-modal-description" sx={{ mt: 2 }}>
-                            Duis mollis, est non commodo luctus, nisi erat porttitor ligula.
-    </Typography>
+                            Feedback submitted!
+                        </Typography>
+
+                        <Link onClick={clearResponses} to="/" ><Button variant="contained">Record another response</Button></Link>
+                        <Button variant="contained" onClick={handleClose}>Close</Button>
+
                     </Box>
-                </Modal> */}
+                </Modal>
             </div>
 
         </div >
